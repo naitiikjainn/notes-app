53e0ef07e74d5dff5a23e911f7e1cc65b7835dac
Naitik Jain <150283488+naitiikjainn@users.noreply.github.com>
2024-10-20 14:41:08 +0530

    created file structure installed required dependencies in pom xml file

---
+++ src/main/java/com/example/demo/entity/note.java
@@ -0,0 +1,51 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    public note() {
+    }
+
+    public note(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
